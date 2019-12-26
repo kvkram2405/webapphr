e7ae63375f775483454a643e450211760011c814
RAM <kvkram2405@gmail.com>
2019-12-26 18:41:11 +0530

    Port has been added

--- webapphr/webapphr.go
+++ webapphr/webapphr.go
@@ -1,28 +1,30 @@
 package main
+
 import (
-  "log"
-  "fmt"
-  "net/http"
-  "os"
+	"fmt"
+	"log"
+	"net/http"
 )
+
 func determineListenAddress() (string, error) {
-  port := os.Getenv("PORT")
-  if port == "" {
-    return "", fmt.Errorf("$PORT not set")
-  }
-  return ":" + port, nil
+	//port := os.Getenv("PORT")
+	port := "8080"
+	if port == "" {
+		return "", fmt.Errorf("$PORT not set")
+	}
+	return ":" + port, nil
 }
 func hello(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "Hello World")
+	fmt.Fprintln(w, "Hello World")
 }
 func main() {
-  addr, err := determineListenAddress()
-  if err != nil {
-    log.Fatal(err)
-  }
-  http.HandleFunc("/", hello)
-  log.Printf("Listening on %s...\n", addr)
-  if err := http.ListenAndServe(addr, nil); err != nil {
-    panic(err)
-  }
+	addr, err := determineListenAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.HandleFunc("/", hello)
+	log.Printf("Listening on %s...\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		panic(err)
+	}
 }
