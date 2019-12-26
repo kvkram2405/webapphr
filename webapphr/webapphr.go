8beeabbaf43ab1e4c1f8a626a007302c921fbedc
RAM <kvkram2405@gmail.com>
2019-12-26 18:08:03 +0530

    New file

--- webapphr/webapphr.go
+++ webapphr/webapphr.go
@@ -1,31 +1,28 @@
 package main
-
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+  "log"
+  "fmt"
+  "net/http"
+  "os"
 )
-
+func determineListenAddress() (string, error) {
+  port := os.Getenv("PORT")
+  if port == "" {
+    return "", fmt.Errorf("$PORT not set")
+  }
+  return ":" + port, nil
+}
+func hello(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "Hello World")
+}
 func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("listening...")
-	err := http.ListenAndServe(GetPort(), nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello. This is our first Go web app on Heroku!")
-}
-
-func GetPort() string {
-	var port = os.Getenv("PORT")
-	// Set a default port if there is nothing in the environment
-	if port == "" {
-		port = "4747"
-		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
-	}
-	return ":" + port
+  addr, err := determineListenAddress()
+  if err != nil {
+    log.Fatal(err)
+  }
+  http.HandleFunc("/", hello)
+  log.Printf("Listening on %s...\n", addr)
+  if err := http.ListenAndServe(addr, nil); err != nil {
+    panic(err)
+  }
 }
